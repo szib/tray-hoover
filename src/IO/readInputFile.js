c7db5d9046beec736a0a2d7c4d070d4e0195cac6
Ivan Szebenszki <szib77@gmail.com>
2019-10-07 17:25:32 +0100

    Add fileIO

---
+++ src/IO/readInputFile.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import util from 'util';
+
+import FileError from './FileError';
+
+const readFile = util.promisify(fs.readFile);
+
+export function validateInput(fileContent) {
+  const regex = /^(\d{1,} \d{1,}\n){2,}[NEWS]*$/;
+  if (!fileContent.match(regex)) {
+    throw new FileError('Invalid file format');
+  } else {
+    return fileContent;
+  }
+}
+
+export async function readInputFile(file) {
+  try {
+    const data = await readFile(file, 'utf8');
+    return data;
+  } catch (error) {
+    throw new FileError('Cannot read file');
+  }
+}
+
+export function getInput(filename) {
+  return readInputFile(filename).then(validateInput);
+}
